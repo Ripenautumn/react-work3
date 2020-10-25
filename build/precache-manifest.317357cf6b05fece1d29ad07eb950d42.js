db96044a18eb0a4015747d45c451facc146a0963
赵嘉怡 <44658478+Ripenautumn@users.noreply.github.com>
2020-10-25 20:03:37 +0800

    Add files via upload

---
+++ build/precache-manifest.317357cf6b05fece1d29ad07eb950d42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56149bc217bf782bca66a93c859d6e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "747fb479bb0aa24158c3",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "747fb479bb0aa24158c3",
+    "url": "/static/js/2.71f64111.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.71f64111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4e557822e06faa39da",
+    "url": "/static/js/main.fda3d100.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "8703f2de59cf9d6fae5f71958e7bbe24",
+    "url": "/static/media/banner.8703f2de.jpg"
+  }
+]);
